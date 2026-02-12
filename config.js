0d8cf0cb2b82a304517daf06e285aaa72d6001e3
CAVaibhav171 <goelcavaibhav@gmail.com>
2026-02-13 01:27:10 +0530

    Update celebration message in config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Ishq tera rang ajab chadha,Main apna aap hi bhool gaya,Log puchde ne haal mera, Main muskura ke kehnda…Jisda naam lenda haan, main oh hi ho gaya.",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
